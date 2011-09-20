3c6268f60350e787f84cd8e21845fa625ba85d0d
Hugo Barauna <hugo.barauna@plataformatec.com.br>
2011-09-20 09:37:57 -0300

    Creates the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobboard::Application.config.secret_token = '4dfb61285d05ca83afafb8322fc594c160492531a66171111548220691448f0de8ac07cb01631528772c38c4f9467b9102450066d18730f8a2695c76b37295a0'
